63e05c7b587c035c47ca6347bccb379883895a9e
AlcindoSchleder <alcindoschleder@gmail.com>
2019-11-21 11:10:03 -0200

    commit final da apresentação

--- taf_car/settings.py
+++ taf_car/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', True)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', ['localhost', '127.0.0.1'])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', 'localhost,127.0.0.1')
+ALLOWED_HOSTS = ALLOWED_HOSTS.split(',')
+
+#print('mostrando hosts:', ALLOWED_HOSTS, 'type: ', type(ALLOWED_HOSTS))
 
 # Application definition
 

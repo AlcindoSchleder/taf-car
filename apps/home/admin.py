9eb1c6af5e7bcc558845b80c17e1cd4e71325be6
Alcindo <alcindoschleder@gmail.com>
2020-02-21 16:28:29 -0300

    mostrando banco de dados no admin

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from .models import Cars, CarsBoxes
+from .models import Cars, CarsBoxes, CarBoxesMessage
 
 # Register your models here.
 admin.site.register(Cars)
 admin.site.register(CarsBoxes)
+admin.site.register(CarBoxesMessage)

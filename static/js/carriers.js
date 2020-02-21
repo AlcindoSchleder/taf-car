a6b9e5d45310b2f6e57467e8fdfe9a82259f1b7e
Alcindo <alcindoschleder@gmail.com>
2020-02-20 22:21:42 -0300

    datatype para ajax deve ser string e não json

--- static/js/carriers.js
+++ static/js/carriers.js
@@ -53,7 +53,7 @@ var IndexEvents = function () {
         $.ajax({
             type: "GET",
             url: 'http://192.168.0.203/api/mqtt/send_message/' + message + '/' + display,
-            dataType: 'text'
+            dataType: 'text',
             success: function(d) {
                 d = JSON.parse(d);
                 console.log(d)

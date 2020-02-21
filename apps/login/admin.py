966af0020e9b827e31d6a9366afef0d0c755a4dd
Alcindo <alcindoschleder@gmail.com>
2020-02-21 16:15:27 -0300

    ajustando banco de dados para aceitar código da caixa nulos

--- apps/login/admin.py
+++ apps/login/admin.py
@@ -1,6 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
-from apps.home.models import Cars, CarsBoxes
 
 # Register your models here.
-admin.site.register(Cars)
-admin.site.register(CarsBoxes)

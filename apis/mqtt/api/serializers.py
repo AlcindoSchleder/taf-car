187a771255c808025a6c55c3bfb21a0da19a0864
Alcindo <alcindoschleder@gmail.com>
2020-02-20 21:33:45 -0300

    retirei as credenciais por enquanto para testes

--- apis/mqtt/api/serializers.py
+++ apis/mqtt/api/serializers.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 from rest_framework import serializers
-from taxes.models import Taxes
+# from taxes.models import Taxes
 
 
-class TaxesSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Taxes
-        fields = ['country', 'province', 'tax']
+# class TaxesSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Taxes
+#         fields = ['country', 'province', 'tax']
